d1162ae3da79753afb348a1eb4b0d5461b95fed1
swarajmhatre <swarajmhatre25@gmail.com>
2023-07-18 18:45:32 +0530

    18th July

--- Problems/Program441.cpp
+++ Problems/Program441.cpp
@@ -56,52 +56,205 @@ public:
 };
 
 template <class T>
-SinglyLL<T>::SinglyLL() {}
+SinglyLL<T>::SinglyLL()
+{
+    first = NULL;
+    iCount = 0;
+}
 
 template <class T>
 void SinglyLL<T>::Display()
 {
+    cout << "Elements of Singly Linear Linked List are: \n";
+
+    NodeS<T> *temp = first;
+
+    while (temp != NULL)
+    {
+        cout << "|" << temp->data << "| -> ";
+        temp = temp->next;
+    }
+    cout << "NULL"
+         << "\n";
 }
 
 template <class T>
 int SinglyLL<T>::Count()
 {
-    return 0;
+    return iCount;
 }
 
 template <class T>
 void SinglyLL<T>::InsertFirst(T no)
 {
+    NodeS<T> *newn = new NodeS<T>;
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if (first == NULL)
+    {
+        first = newn;
+    }
+    else
+    {
+        newn->next = first;
+        first = newn;
+    }
+    iCount++;
 }
 
 template <class T>
 void SinglyLL<T>::InsertLast(T no)
 {
+    NodeS<T> *newn = new NodeS<T>;
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if (first == NULL)
+    {
+        first = newn;
+    }
+    else
+    {
+        NodeS<T> *temp = first;
+
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+
+    iCount++;
 }
 
 template <class T>
 void SinglyLL<T>::InsertAtPos(T no, int iPos)
 {
+    int iCount = Count();
+    if ((iPos < 1) || (iPos > iCount + 1))
+    {
+        cout << "Invalid Position!\n";
+        return;
+    }
+
+    if (iPos == 1)
+    {
+        InsertFirst(no);
+    }
+    else if (iPos == iCount + 1)
+    {
+        InsertLast(no);
+    }
+    else
+    {
+
+        NodeS<T> *newn = new NodeS<T>;
+
+        newn->data = no;
+        newn->next = NULL;
+        NodeS<T> *temp = first;
+
+        for (int iCnt = 1; iCnt < iPos - 1; iCnt++)
+        {
+            temp = temp->next;
+        }
+        newn->next = temp->next;
+        temp->next = newn;
+
+        iCount++;
+    }
 }
 
 template <class T>
 void SinglyLL<T>::DeleteFirst()
 {
+    if (first == NULL)
+    {
+        return;
+    }
+    else if (first->next == NULL)
+    {
+        delete first;
+        first = NULL;
+    }
+    else
+    {
+        NodeS<T> *temp = first;
+
+        first = first->next;
+        delete temp;
+    }
+    iCount--;
 }
 
 template <class T>
 void SinglyLL<T>::DeleteLast()
 {
+    if (first == NULL)
+    {
+        return;
+    }
+    else if (first->next == NULL)
+    {
+        delete first;
+        first = NULL;
+    }
+    else
+    {
+        NodeS<T> *temp = first;
+
+        while (temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        delete temp->next;
+        temp->next = NULL;
+    }
+    iCount--;
 }
 
 template <class T>
 void SinglyLL<T>::DeleteAtPos(int iPos)
 {
+    int iCount = Count();
+    if ((iPos < 1) || (iPos > iCount))
+    {
+        cout << "Invalid Position!\n";
+        return;
+    }
+
+    if (iPos == 1)
+    {
+        DeleteFirst();
+    }
+    else if (iPos == iCount + 1)
+    {
+        DeleteLast();
+    }
+    else
+    {
+
+        NodeS<T> *temp1 = first;
+        NodeS<T> *temp2 = first;
+
+        for (int iCnt = 1; iCnt < iPos - 1; iCnt++)
+        {
+            temp1 = temp1->next;
+        }
+        temp2 = temp1->next->next;
+        delete temp1->next;
+        temp1->next = temp2;
+
+        iCount--;
+    }
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Class of SIngly Circluar LL
+// Class of Singly Circluar LL
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -128,17 +281,37 @@ public:
 };
 
 template <class T>
-SinglyCL<T>::SinglyCL() {}
+SinglyCL<T>::SinglyCL()
+{
+    first = NULL;
+    last = NULL;
+    iCount = 0;
+}
 
 template <class T>
 void SinglyCL<T>::Display()
 {
+
+    if ((first != NULL) && (last != NULL))
+    {
+        NodeS *temp = first; 
+        do
+        {
+            printf("| %d | =>", temp->data);
+            temp = temp->next;
+        } while (temp != last->next);
+        printf("\n");
+    }
+    else
+    {
+        printf("Linked List is Empty!");
+    }
 }
 
 template <class T>
 int SinglyCL<T>::Count()
 {
-    return 0;
+    return iCount;
 }
 
 template <class T>
@@ -313,7 +486,6 @@ template <class T>
 void DoublyCL<T>::DeleteAtPos(int iPos)
 {
 }
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
